558572a7c830de3722149f938a387fe0f8d5c2a2
shiori.hidaka <shiori.a.hidaka@rakuten.com>
2018-06-25 03:35:39 +0000

    kadai

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -29,11 +29,9 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a href="{{ route('ranking.want') }}">Wantランキング</a></li>
-                            </ul>
-                            
-                             <ul class="dropdown-menu">
                                 <li><a href="{{ route('ranking.have') }}">Haveランキング</a></li>
                             </ul>
+                            
                         </li>
                         
                         <li class="dropdown">
